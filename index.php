420f6bcdb21a2c0445ea176f6899bc232ecaf9db
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2024-02-13 18:16:04 -0300

    Esqueci a senha

--- index.php
+++ index.php
@@ -158,6 +158,76 @@ $app->post('/admin/users/:iduser', function($iduser) {
 });
 
 
+$app->get('/admin/forgot', function() {
+    
+	$page = new PageAdmin([  # desabilita header e footer
+		"header"=>false,
+		"footer"=>false,
+	]);
+
+	$page->setTpl("forgot");
+});
+
+
+$app->post('/admin/forgot', function() {
+    
+	$user = User::getForgot($_POST["email"]); # recupera e-mail
+	
+	header("Location: /admin/forgot/sent");
+	exit;
+});
+
+
+$app->get("/admin/forgot/sent", function(){
+	
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-sent");
+});
+
+
+$app->get("/admin/forgot/reset", function(){
+		
+	$user = User::validForgotDecrypt($_GET["code"]);
+	
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+	));
+});
+
+
+$app->post("/admin/forgot/reset", function(){
+	
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+	
+	User::setForgotUsed($forgot["idrecovery"]); # verifica se processo de recuperacao ja foi usado
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [ # hash da senha
+		"cost"=>12
+	]);
+
+	$user->setPassword($password); # salva nova senha
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset-success");
+});
 
 
 $app->run();
--- vendor/joaovictorbs/php-classes/src/Model/User.php
+++ vendor/joaovictorbs/php-classes/src/Model/User.php
@@ -2,12 +2,18 @@
 
 namespace Joaovictorbs\Model;
 
+use Exception;
 use \Joaovictorbs\DB\Sql;
 use \Joaovictorbs\Model;
+use \Joaovictorbs\Mailer;
 
 Class User extends Model{
 
     const SESSION = "User"; # constante para sessao dos dados
+    const SECRET = ""; # constante para chave de recuperacao de senha / deve ser 16 bits
+	const SECRET_IV = "";
+
+
 
     public static function login($login, $password) 
     {
@@ -126,6 +132,103 @@ Class User extends Model{
             ":iduser"=>$this->getiduser(),
         ));
     }
+
+
+    public static function getForgot($email)
+    {
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_persons a INNER JOIN tb_users b USING(idperson) WHERE a.desemail = :email", array(
+            ":email"=>$email
+        ));
+
+        if (count($results) === 0) {
+            throw new \Exception("Não foi possível recuperar a senha.", 1);
+        }
+        else {
+            $data = $results[0];
+            
+            $results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array( # salva nova tentativa de recuperar senha
+                ":iduser"=>$data["iduser"],
+                ":desip"=>$_SERVER["REMOTE_ADDR"] # recupera ip do usuario
+            ));
+
+            if (count($results2) === 0) {
+                throw new \Exception("Não foi possível recuperar a senha.", 1);
+            }
+            else {
+                $dataRecovery = $results2[0];
+
+                $code = openssl_encrypt($dataRecovery['idrecovery'], 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+				$code = base64_encode($code);
+
+                $link = "http://www.projetoecommercephp.com.br/admin/forgot/reset?code=$code";
+
+                $mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir senha E-commerce Store", "forgot",
+                 array(
+                    "name"=>$data["desperson"],
+                    "link"=>$link
+                ));
+
+                $mailer->send();
+
+                return $data;
+            }
+        }
+
+    }
+
+
+    public static function validForgotDecrypt($code) # decodifica codigo de recuperacao de senha
+    {
+		$code = base64_decode($code);
+
+		$idrecovery = openssl_decrypt($code, 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_userspasswordsrecoveries a
+                                 INNER JOIN tb_users b USING(iduser)
+                                 INNER JOIN tb_persons c USING(idperson)
+                                 WHERE
+                                     a.idrecovery = :idrecovery
+                                 AND
+                                     A.dtrecovery IS NULL
+                                 AND
+                                     DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();
+                                ", array(
+                                    ":idrecovery"=>$idrecovery
+                                ));
+
+        if (count($results) === 0) {
+            throw new \Exception("Não foi possível recuperar a senha.");
+        }
+        else {
+            return $results[0]; # retorna dados do usuario
+        }
+    }
+
+
+    public static function setForgotUsed($idrecovery) # atualiza tabela de recuperacao de senha
+    {
+        $sql = new Sql();
+
+        $sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+            ":idrecovery"=>$idrecovery
+        ));
+    }
+
+    public function setPassword($password) # atualiza senha do usuario
+    {
+        $sql = new Sql();
+
+        $sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
+            "password"=>$password,
+            "iduser"=>$this->getiduser()
+        ));
+    }
+
 }
 
 ?>
